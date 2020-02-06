07a6afe5fd38a3e88af983b9d0f5adc39fc54d39
Margiett Gil <mxg627@icloud.com>
2020-02-06 10:49:17 -0500

    Initial Commit

---
+++ NYCTopStories/TopStoriesTabController.swift
@@ -0,0 +1,20 @@
+//
+//  TopStoriesTabController.swift
+//  NYCTopStories
+//
+//  Created by Margiett Gil on 2/6/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+ 
+}
