7d8a99a393eb0ba278abe82d86e9787f6dfa878e
Margiett Gil <mxg627@icloud.com>
2020-02-06 12:55:06 -0500

    added api

--- NYCTopStories/NetworkHelper/TopStoriesAPIClient.swift
+++ NYCTopStories/NetworkHelper/TopStoriesAPIClient.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct NYTopStoriesAPIClient
+{
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        let endPointURLString = ""
+        guard let url = URL(string: endPointURLString) else {
+            completion(.failure(.badURL(endPointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let AppError):
+                completion(.failure(.networkClientError(AppError)))
+            case .success(let data):
+                do{
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(topStories.results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
+
+
